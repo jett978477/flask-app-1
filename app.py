dec284346765c56137658eebf8d51f679c5f8899
jett978477 <jett978477@my.yosemite.edu>
2025-12-19 22:13:54 -0800

    mostly done

--- app.py
+++ app.py
@@ -1,36 +1,51 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit, send
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, emit, join_room, leave_room
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+# --- ROUTES ---
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/chat/<room>')
+def chat(room):
+    if room.lower() not in ['a', 'b', 'c', 'd']:
+        return redirect(url_for('index'))
+    return render_template('page1.html', room_id=room.upper())
+
+@app.route('/settings')
+def settings():
+    return render_template('page2.html')
+
+# --- SOCKETS ---
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    # emit('system_msg', {'msg': f'{username} joined.'}, to=room)
 
 @socketio.on('message')
 def handle_message(data):
     msg = data['msg']
     username = data['username']
+    room = data['room']
+    color = data.get('color', '#000000')
+    
+    # Capture the time the message arrived at server
+    timestamp = time.time() 
 
-    # Command Logic
-    if msg.startswith('/'):
-        command = msg.split(' ')[0].lower()
-        
-        if command == '/help':
-            emit('system_msg', {'msg': 'Commands: /help, /ping, /name [newname]'})
-        elif command == '/ping':
-            emit('system_msg', {'msg': 'Pong!'})
-        elif command == '/name':
-            new_name = msg.split(' ', 1)[1] if ' ' in msg else "Anonymous"
-            emit('system_msg', {'msg': f'System: Changing name to {new_name}'})
-            emit('change_name', {'new_name': new_name})
-        else:
-            emit('system_msg', {'msg': 'Unknown command. Type /help.'})
-    else:
-        # Normal chat broadcast to everyone
-        emit('chat_msg', {'username': username, 'msg': msg}, broadcast=True)
+    emit('chat_msg', {
+        'username': username, 
+        'msg': msg, 
+        'color': color,
+        'server_time': timestamp # Send this back to client
+    }, to=room)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
